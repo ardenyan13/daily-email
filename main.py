129183a0d51b2c05415d040624b83129f8368cd3
ardenyan13 <148033490+ardenyan13@users.noreply.github.com>
2024-01-24 23:14:42 -0800

    Send an email with news titles and descriptions

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
+from send_email import send_email
 
 api_key = "086db17afc0b4c3280600bfe1934e0da"
 url = "https://newsapi.org/v2/everything?q=tesla&from=2023-12-25&sortBy=publishedAt&" \
-      "apiKey=" + api_key
+      "apiKey=086db17afc0b4c3280600bfe1934e0da"
 
 # make request
 request = requests.get(url)
@@ -11,6 +12,11 @@ request = requests.get(url)
 content = request.json()
 
 # get the article titles and description
+message = ""
 for article in content["articles"]:
-      print(article["title"])
-      print(article["description"])
+    # some articles may have null titles or null descriptions
+    if article["title"] and article["description"]:
+        message = message + article["title"] + "\n" + article["description"] + "\n\n"
+
+message = message.encode("utf-8")
+send_email(message)
