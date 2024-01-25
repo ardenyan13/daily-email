721c99eeaf37fae75e4dc49c0aef999f5b58520c
ardenyan13 <148033490+ardenyan13@users.noreply.github.com>
2024-01-24 23:28:46 -0800

    Add links, language selection, limit the amount of news, add subject line to email, and control topic

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import requests
 from send_email import send_email
 
+topic = "tesla"
 api_key = "086db17afc0b4c3280600bfe1934e0da"
-url = "https://newsapi.org/v2/everything?q=tesla&from=2023-12-25&sortBy=publishedAt&" \
-      "apiKey=086db17afc0b4c3280600bfe1934e0da"
+url = ("https://newsapi.org/v2/everything?"
+       f"q={topic}&"
+       "sortBy=publishedAt&"
+       "apiKey=086db17afc0b4c3280600bfe1934e0da&"
+       "language=en")
 
 # make request
 request = requests.get(url)
@@ -12,11 +16,12 @@ request = requests.get(url)
 content = request.json()
 
 # get the article titles and description
-message = ""
-for article in content["articles"]:
+message = "Subject: Today's news \n"
+for article in content["articles"][:20]:
     # some articles may have null titles or null descriptions
     if article["title"] and article["description"]:
-        message = message + article["title"] + "\n" + article["description"] + "\n\n"
+        message += (article["title"] + "\n" + article["description"] + "\n"
+                    + article["url"] + "\n\n")
 
 message = message.encode("utf-8")
 send_email(message)
